86121a15fb137c14b9119aaaa1f09bcf4c59d46d
Rae Bouldin <rbouldin@vt.edu>
2020-04-23 17:14:27 -0400

    add edit password option

--- src/NavigationMenu.java
+++ src/NavigationMenu.java
@@ -18,12 +18,13 @@ public class NavigationMenu {
 	public static final int SIGN_OUT = 5;
 	
 	/* Return options for displayEditProfile()*/
-	public static final int EDIT_FIRST_NAME = 1;
-	public static final int EDIT_LAST_NAME = 2;
-	public static final int EDIT_EMAIL = 3;
-	public static final int EDIT_PHONE_NUMBER = 4;
-	public static final int EDIT_MAJOR = 5;
-	public static final int EDIT_YEAR = 6;
+	public static final int EDIT_EMAIL = 1;
+	public static final int EDIT_PASSWORD = 2;
+	public static final int EDIT_FIRST_NAME = 3;
+	public static final int EDIT_LAST_NAME = 4;
+	public static final int EDIT_PHONE_NUMBER = 5;
+	public static final int EDIT_MAJOR = 6;
+	public static final int EDIT_YEAR = 7;
 	
 	public static int displayMainMenu(Scanner scan, User user) 
 	{
@@ -79,40 +80,44 @@ public class NavigationMenu {
 		}
 		
 		if (user.getClass() == StudentUser.class) {
-			System.out.println("1. Edit First Name");
-			System.out.println("2. Edit Last Name");
-			System.out.println("3. Edit Email");
-			System.out.println("4. Edit Phone Number");
-			System.out.println("5. Edit Major");
-			System.out.println("6. Edit Year");
-			System.out.println("7. Cancel");
+			System.out.println("1. Edit Email");
+			System.out.println("2. Edit Password");
+			System.out.println("3. Edit First Name");
+			System.out.println("4. Edit Last Name");
+			System.out.println("5. Edit Phone Number");
+			System.out.println("6. Edit Major");
+			System.out.println("7. Edit Year");
+			System.out.println("8. Cancel");
 			
 			switch (choice = scan.nextInt()) {
-				case 1: return EDIT_FIRST_NAME;
-				case 2: return EDIT_LAST_NAME;
-				case 3: return EDIT_EMAIL;
-				case 4: return EDIT_PHONE_NUMBER;
-				case 5: return EDIT_MAJOR;
-				case 6: return EDIT_YEAR;
-				case 7: return CANCEL;
+				case 1: return EDIT_EMAIL;
+				case 2: return EDIT_PASSWORD;
+				case 3: return EDIT_FIRST_NAME;
+				case 4: return EDIT_LAST_NAME;
+				case 5: return EDIT_PHONE_NUMBER;
+				case 6: return EDIT_MAJOR;
+				case 7: return EDIT_YEAR;
+				case 8: return CANCEL;
 				default:
 					// unrecognized input, throw error
 					break;
 			}
 		}
 		else {
-			System.out.println("1. Edit First Name");
-			System.out.println("2. Edit Last Name");
-			System.out.println("3. Edit Email");
-			System.out.println("4. Edit Phone Number");
-			System.out.println("5. Cancel");
+			System.out.println("1. Edit Email");
+			System.out.println("2. Edit Password");
+			System.out.println("3. Edit First Name");
+			System.out.println("4. Edit Last Name");
+			System.out.println("5. Edit Phone Number");
+			System.out.println("6. Cancel");
 			
 			switch (choice = scan.nextInt()) {
-				case 1: return EDIT_FIRST_NAME;
-				case 2: return EDIT_LAST_NAME;
-				case 3: return EDIT_EMAIL;
-				case 4: return EDIT_PHONE_NUMBER;
-				case 5: return CANCEL;
+				case 1: return EDIT_EMAIL;
+				case 2: return EDIT_PASSWORD;
+				case 3: return EDIT_FIRST_NAME;
+				case 4: return EDIT_LAST_NAME;
+				case 5: return EDIT_PHONE_NUMBER;
+				case 6: return CANCEL;
 				default:
 					// unrecognized input, throw error
 					break;
