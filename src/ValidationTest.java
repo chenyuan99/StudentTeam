21b88f4dc9154605a42e065e8fd7708598a0ab54
Yuan Chen <18861720589@163.com>
2020-04-23 14:39:44 -0400

    Create ValidationTest.java

---
+++ src/ValidationTest.java
@@ -0,0 +1,74 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * 
+ */
+
+/**
+ * @author cheny
+ *
+ */
+class ValidationTest {
+
+	private MainDriver mD;
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception {
+		mD = new MainDriver();
+	}
+
+	/**
+	 * Test method for {@link MainDriver#main(java.lang.String[])}.
+	 */
+	@Test
+	void testMain() {
+		// main();
+	}
+
+	/**
+	 * Test method for {@link MainDriver#main(java.lang.String[])}.
+	 */
+	@Test
+	void testUserCase1() {
+		// main();
+	}
+
+	/**
+	 * Test method for {@link MainDriver#main(java.lang.String[])}.
+	 */
+	@Test
+	void testUserCase2() {
+		// main();
+	}
+
+	/**
+	 * Test method for {@link MainDriver#main(java.lang.String[])}.
+	 */
+	@Test
+	void testUserCase3() {
+		// main();
+	}
+
+	/**
+	 * Test method for {@link MainDriver#main(java.lang.String[])}.
+	 */
+	@Test
+	void testUserCase4() {
+		// main();
+	}
+
+	/**
+	 * Test method for {@link MainDriver#main(java.lang.String[])}.
+	 */
+	@Test
+	void testUserCase5() {
+		// main();
+	}
+
+}
