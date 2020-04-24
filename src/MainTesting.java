dff291f45a69910045e1c3cbc4ed9a93b8c37373
Yuan Chen <18861720589@163.com>
2020-04-23 20:02:18 -0400

    update

---
+++ src/MainTesting.java
@@ -0,0 +1,27 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class MainTesting {
+    
+    private String fileName;
+    
+    @BeforeEach
+    void setUp() throws Exception {
+        fileName = "MainTest.txt";
+    }
+
+
+    @Test
+    // running all the user input possibility to make sure, no hiding errors
+    void test() throws FileNotFoundException {     
+        String[] args = null;
+        System.setIn(new FileInputStream(fileName));
+        MainDriver.main(args);
+        
+    }
+    
+
+}
