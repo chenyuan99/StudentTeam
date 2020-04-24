46edc50f4a9120650fb9e3c0f7c4c174056971f7
Yuan Chen <18861720589@163.com>
2020-04-23 20:22:37 -0400

    update validation tests

--- src/ValidationTest.java
+++ src/ValidationTest.java
@@ -1,5 +1,8 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -14,6 +17,7 @@ import org.junit.jupiter.api.Test;
 class ValidationTest {
 
 	private MainDriver mD;
+	private String fileName;
 
 	/**
 	 * @throws java.lang.Exception
@@ -25,50 +29,80 @@ class ValidationTest {
 
 	/**
 	 * Test method for {@link MainDriver#main(java.lang.String[])}.
+	 * 
+	 * @throws FileNotFoundException
 	 */
 	@Test
-	void testMain() {
-		// main();
+	void testMain() throws FileNotFoundException {
+		fileName = "MainTest.txt";
+		String[] args = null;
+		System.setIn(new FileInputStream(fileName));
+		MainDriver.main(args);
 	}
 
 	/**
 	 * Test method for {@link MainDriver#main(java.lang.String[])}.
+	 * 
+	 * @throws FileNotFoundException
 	 */
 	@Test
-	void testUserCase1() {
-		// main();
+	void testUserCase1() throws FileNotFoundException {
+		fileName = "uc1-register.txt";
+		String[] args = null;
+		System.setIn(new FileInputStream(fileName));
+		MainDriver.main(args);
 	}
 
 	/**
 	 * Test method for {@link MainDriver#main(java.lang.String[])}.
+	 * 
+	 * @throws FileNotFoundException
 	 */
 	@Test
-	void testUserCase2() {
-		// main();
+	void testUserCase2() throws FileNotFoundException {
+		fileName = "uc2-signin.txt";
+		String[] args = null;
+		System.setIn(new FileInputStream(fileName));
+		MainDriver.main(args);
 	}
 
 	/**
 	 * Test method for {@link MainDriver#main(java.lang.String[])}.
+	 * 
+	 * @throws FileNotFoundException
 	 */
 	@Test
-	void testUserCase3() {
-		// main();
+	void testUserCase3() throws FileNotFoundException {
+		fileName = "uc3-modify.txt";
+		String[] args = null;
+		System.setIn(new FileInputStream(fileName));
+		MainDriver.main(args);
 	}
 
 	/**
 	 * Test method for {@link MainDriver#main(java.lang.String[])}.
+	 * 
+	 * @throws FileNotFoundException
 	 */
 	@Test
-	void testUserCase4() {
-		// main();
+	void testUserCase4() throws FileNotFoundException {
+		fileName = "uc4-logout.txt";
+		String[] args = null;
+		System.setIn(new FileInputStream(fileName));
+		MainDriver.main(args);
 	}
 
 	/**
 	 * Test method for {@link MainDriver#main(java.lang.String[])}.
+	 * 
+	 * @throws FileNotFoundException
 	 */
 	@Test
-	void testUserCase5() {
-		// main();
+	void testUserCase5() throws FileNotFoundException {
+		fileName = "uc5-removal.txt";
+		String[] args = null;
+		System.setIn(new FileInputStream(fileName));
+		MainDriver.main(args);
 	}
 
 }
