4f8d2ed9e5a2270f3acedf1040b8aa1956c5af7d
safranklin <37414077+safranklin@users.noreply.github.com>
2020-04-23 22:00:11 -0400

    Add test case for remove

--- src/DatabaseTest.java
+++ src/DatabaseTest.java
@@ -14,16 +14,24 @@ public class DatabaseTest {
 
     @Test
     void testAddingDatabase() {
-
         User testUser = new User(1, "John","Doe", "jdoe@vt.edu", "0001110000", "janewhereareyou");
         assertEquals(db.store(testUser.getEmail(),testUser), testUser);
-
     }
 
     @Test
     void testLoadingDatabase() {
         db.loadDatabase();
         assertEquals(db.retrieve("jdoe@vt.edu").getFirstName().equals("John"), true);
-
     }
+
+
+    @Test
+    void testRemoveFromDatabase() {
+        User testUser = new User(1, "John","Doe", "jdoe@vt.edu", "0001110000", "janewhereareyou");
+        User testUser2 = new User(1, "John","Doe", "jdoe@null.edu", "0001110000", "janewhereareyou");
+        assertEquals(db.remove(testUser2.getEmail()), null);
+        assertEquals(db.remove(testUser.getEmail()).getFirstName(), testUser.getFirstName());
+        assertEquals(db.store(testUser.getEmail(),testUser), testUser); // Add it back in for the other tests that rely on it
+    }
+    
 }
